ce94c0e001d1584c0599eae478985d27803a8fee
Daniel Z <danielzheng1997@gmail.com>
2017-01-24 02:41:44 -0500

    complete

--- js/animation.js
+++ js/animation.js
@@ -21,11 +21,56 @@ function moveElement(theid, percent) {
     }, 10);
 }
 
+var flag = 0;
 $(document).ready(function() {
-    $('a').click(function() {
-        $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top
-        }, 500);
-        return false;
+    var wSize = $(window).height();
+    $('.header_pic').css({
+        'height': wSize
+    });
+    
+    $(window).scroll(function(){
+        var scrollvalue= $(this).scrollTop();
+
+        $('.name').css({
+           'transform' : 'translate(0px, '+ scrollvalue /2 +'%)'
+        });
+
+        
+        
+        if ((scrollvalue > $('#first').offset().top) && (flag == 0)) {
+            flag += 1;
+            move();
+        }
+
+        if (scrollvalue > $('#skills').offset().top - 400) {
+                $('.specify h3').each(function(i){
+                    setTimeout(function(){
+                    $('.specify h3').eq(i).addClass('fadein');
+                }, 150 * (i + 1))
+            });
+        }
+        if (scrollvalue > $('#cc3k').offset().top - 400) {
+            $('#cc3k').addClass('fadein');
+        }
+
+        if (scrollvalue > $('#meetup').offset().top - 400) {
+            $('#meetup').addClass('fadein');
+        }
+
+        if (scrollvalue > $('#personal_web').offset().top - 400) {
+            $('#personal_web').addClass('fadein');
+        }
+    });
+
+
+
+
+    $('a').click(function(e) {
+    e.preventDefault();
+    var linkHref= $(this).attr('href');
+    var headerHeight= $('header').outerHeight();
+         $('html, body').animate({
+             scrollTop: $(linkHref).offset().top - headerHeight
+         }, 500);
     });
 });
